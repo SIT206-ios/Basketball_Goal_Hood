687656357c068620b09f2ba8652615ad13d8d77a
YINNENG DU <yinneng@10-148-140-34.dhcp-m.deakin.edu.au>
2017-04-29 18:22:04 +1000

    1st commit.

---
+++ basketball_goal_hood/basketball_goal_hood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  basketball_goal_hood
+//
+//  Created by YINNENG DU on 29/04/2017.
+//  Copyright © 2017 YINNENG DU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
